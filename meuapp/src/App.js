6e7ed138384ec4302ee659c49e38ba098d42df83
Felipe Xavier <felipe_rsr@live.com>
2022-04-06 16:19:52 -0300

    update

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,60 +1,61 @@
 import React from 'react';
 
-const luana = {
-  nome: 'Luana',
-  idade: 27,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2000' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-  ],
-  ativa: true,
-};
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
 
-const mario = {
-  nome: 'Mario',
-  idade: 24,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-    { nome: 'Guitarra', preco: 'R$ 3500' },
-  ],
-  ativa: false,
-};
 const App = () => {
-  const dados = luana;
-
-  const total = dados.compras
-    .map((item) => Number(item.preco.replace('R$', '')))
-    .reduce((a, b) => a + b);
-  return (
-    <>
-      <p>Nome: {dados.nome}</p>
-      <p>Idade: {dados.idade}</p>
-      <p>
-        Situação: {' '}
-        <span style={{ color: dados.ativa ? 'green' : 'red' }}>
-          {dados.ativa ? 'Ativa' : 'Inativa'}
-        </span>
-      </p>
-      <p>Total de gasto: {total}</p>
-      {total > 10000 && <p>Voce esta gastando de mais</p>}
-    </>
+  // Organize os produtos como mostrado no vídeo
+  // Mostre apenas produtos que forem mais caros que R$ 1500
+  const dados = produtos.filter(
+    ({ preco }) => Number(preco.replace('R$ ', '')) > 1500,
   );
 
-    const obrasFixas = [
-      'Monalisa',
-      'O pensador',
-      'O Grito'
-    ]
-    const obrasVariaveis = [
-      ...obrasFixas,
-      'Santa Ceia',
-      'Abaporou'
-    ]
-
-
+  console.log(dados);
+  return (
+    <>
+      <section>
+        {produtos
+          .filter((produto) => Number(produto.preco.replace('R$ ', '')) > 1500)
+          .map((produto) => (
+            <div key={produto.id}>
+              <h1>{produto.nome}</h1>
+              <p>Preço: {produto.preco}</p>
+              <ul>
+                {produto.cores.map((cor) => (
+                  <li
+                    key={cor}
+                    style={{ backgroundColor: cor, color: 'white' }}
+                  >
+                    {cor}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+      </section>
+    </>
+  );
 };
 
 export default App;
+
+// Organize os produtos como mostrado no vídeo
+// Mostre apenas produtos que forem mais caros que R$ 1500
